f00ab6bd1860106c142a743168dfd66ca1297825
Yana Lunina <yanalunina96@gmail.com>
2025-05-27 00:45:15 +0300

    done react_carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface CarouselProps {
+  images: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProps> = ({
+  images,
+  itemWidth = 130,
+  frameSize = 3,
+  step = 3,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [position, setPosition] = useState(0);
+
+  const maxPosition = images.length - frameSize;
+
+  const handleNext = () => {
+    const newPosition = position + step;
+
+    if (newPosition > maxPosition) {
+      if (infinite) {
+        setPosition(0);
+      }
+    } else {
+      setPosition(newPosition);
+    }
+  };
+
+  const handlePrev = () => {
+    const newPosition = position - step;
+
+    if (newPosition < 0) {
+      if (infinite) {
+        setPosition(maxPosition);
+      }
+    } else {
+      setPosition(newPosition);
+    }
+  };
+
+  return (
+    <div
+      className="carousel"
+      style={{ width: `${frameSize * itemWidth}px`, overflow: 'hidden' }}
+    >
+      <ul
+        className="carousel__list"
+        style={{
+          transform: `translateX(-${position * itemWidth}px)`,
+          transition: `transform ${animationDuration}ms ease`,
+        }}
+      >
+        {images.map((image, index) => (
+          <li key={index} style={{ width: `${itemWidth}px`, flexShrink: 0 }}>
+            <img
+              src={image}
+              alt={`Slide ${index + 1}`}
+              width={itemWidth}
+              style={{ width: `${itemWidth}px`, height: 'auto' }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="carousel__controls">
+        <button type="button" onClick={handlePrev} className="carousel__btn">
+          Prev
+        </button>
+
+        <button
+          type="button"
+          onClick={handleNext}
+          data-cy="next"
+          className="carousel__btn"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
